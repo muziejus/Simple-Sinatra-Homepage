00c185ea13da6f831fa6974f61039a9ad90d115f
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-08-03 05:45:02 -0400

    Have scrollposstyler initiate itself so that the navbar is dark when loading an internal link.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,23 +1,25 @@
 (function($) {
-    "use strict";
+  "use strict";
 
-    $("a[href^='http']").append(" <i class='fa fa-external-link'></i>");
+  $("a[href^='http']").append(" <i class='fa fa-external-link'></i>");
 
-    $("body").scrollspy({ target: "#navbar" });
+  $("body").scrollspy({ target: "#navbar" });
 
-    new WOW().init();
-    
-    // $("a.page-scroll").click(function(event) {
-    //     var $ele = $(this);
-    //     $("html, body").stop().animate({
-    //         scrollTop: ($($ele.attr("href")).offset().top - 60)
-    //     }, 1450, "easeInOutExpo");
-    //     event.preventDefault();
-    // });
-    
-    $(".navbar-collapse ul li a").click(function() {
-        /* always close responsive nav after click */
-        $(".navbar-toggler:visible").click();
-    });
+  ScrollPosStyler.init();
+
+  new WOW().init();
+  
+  // $("a.page-scroll").click(function(event) {
+  //     var $ele = $(this);
+  //     $("html, body").stop().animate({
+  //         scrollTop: ($($ele.attr("href")).offset().top - 60)
+  //     }, 1450, "easeInOutExpo");
+  //     event.preventDefault();
+  // });
+  
+  $(".navbar-collapse ul li a").click(function() {
+    /* always close responsive nav after click */
+    $(".navbar-toggler:visible").click();
+  });
 
 })(jQuery);
