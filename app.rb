75f1d0abde1bc391c8676f4ff6535a355ba8d0ac
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-06-29 12:38:38 -0400

    Remove legacy trash

--- app.rb
+++ app.rb
@@ -38,62 +38,6 @@ class App < Sinatra::Base
     haml :index, :locals => { :nav => nav_array }
   end
 
-  get '/about' do
-    haml :about, :locals => { :nav => nav_array(1), :subheads => subhead_hash(["bio", "name"], [], "about/") }
-  end
-
-  get '/about/*' do
-    # Add this if block
-    if params[:splat].first.empty?
-      redirect '/about'
-    end
-    if /-/.match(params[:splat].first)
-      path = params[:splat].first
-      /-[a-z]*/ =~ path
-      parttohide = Regexp.last_match(0).gsub(/-/, '')
-      path = path.gsub(/-([a-z]*)\//i, '')
-      path = path.gsub(parttohide, '')
-      path = path.gsub(/^/, '/about/')
-      path = path.gsub('//', '/')
-      redirect path
-    else
-      haml :about, :locals => { :nav => nav_array(1), :subheads => subhead_hash(%w(bio name), params[:splat].first.split("/")) }
-    end
-  end
-
-  get '/contact/?' do
-    erb :contact, :locals => { :nav => nav_array(3) }
-  end
-
-  get '/academics' do
-    haml :academics, :locals => { :nav => nav_array(2), :subheads => subhead_hash(["current", "digitalhumanities", "dissertation", "otherinterests", "publishing", "teaching", "tinkering", "tools", "poparticles", "presentations", "selfpublishing", "cartography"], [], "academics/") }
-  end
-
-  get '/academics/*' do
-    if /(poparticles|presentations|selfpublishing|cartography)/.match(params[:splat].first)
-      redirect params[:splat].first.gsub(/^/, '/academics/publishing/') unless /(\/publishing|^publishing)/.match(params[:splat].first)
-    end
-    if /-/.match(params[:splat].first)
-      path = params[:splat].first
-      if /-publishing/.match(path)
-        path = path.gsub(/(poparticles|presentations|selfpublishing|cartography)\//, '')
-      end
-      /-[a-z]*/ =~ path
-      parttohide = Regexp.last_match(0).gsub(/-/, '')
-      path = path.gsub(/-([a-z]*)\//i, '')
-      path = path.gsub(parttohide, '')
-      path = path.gsub(/^/, '/academics/')
-      path = path.gsub('//', '/')
-      redirect path
-    else
-      haml :academics, :locals => { :nav => nav_array(2), :subheads => subhead_hash(%w(current digitalhumanities dissertation otherinterests publishing teaching tinkering tools poparticles presentations selfpublishing cartography), params[:splat].first.split("/")) }
-    end
-  end
-
-  get '/calendar' do
-    haml :calendar, :locals => { :nav => nav_array }
-  end
-
   get '/talks/mla-15-geocritical-explorations-inside-the-text' do
     haml :mla_15_geocritical, :layout => :layout_mla, :locals => { :nav  => nav_array(2), :subheads => subhead_hash(%w(bios abstracts proposal /talks/mla-15-geocritical-explorations-inside-the-text), []) }
   end
@@ -197,8 +141,6 @@ class App < Sinatra::Base
   error do
       'Sorry there was a nasty error - ' + env['sinatra.error'].name
   end
-
-  # require './bday_mail'
 
 end
 
